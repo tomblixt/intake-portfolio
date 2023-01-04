1b37ae9a840b7de1ade0821fded2765381d46bba
Tom van Merrienboer <tom@merrienboer.eu>
2023-01-04 11:29:57 +0100

    Update project

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,114 +1,138 @@
-import gsap, { Power2 } from 'gsap'
+import gsap, { Power2 } from "gsap";
 
 export default class Home {
   constructor(container) {
     // pages
-    this.pages = container.querySelector('.pages')
-    this.links = this.pages.querySelectorAll('.page-link')
-    this.pagesCursor = container.querySelector('.pages-cursor')
+    this.pages = container.querySelector(".pages");
+    this.links = this.pages.querySelectorAll(".page-link");
+    this.pagesCursor = container.querySelector(".pages-cursor");
     // intro
-    this.intro = container.querySelector('.intro')
-    this.introTitle = this.intro.querySelector('.intro__title')
-    this.revealWrapper = this.introTitle.querySelector('.reveal-wrapper')
-    this.revealText = this.revealWrapper.querySelector('h1')
+    this.intro = container.querySelector(".intro");
+    this.introTitle = this.intro.querySelector(".intro__title");
+    this.revealWrapper = this.introTitle.querySelector(".reveal-wrapper");
+    this.revealText = this.revealWrapper.querySelector("h1");
     // mouse
-    this.screenCenter = { x: window.innerWidth / 2, y: window.innerHeight / 2 }
-    this.mouse = { x: window.innerWidth / 2, y: window.innerHeight / 2 }
-    this.isHovering = false
+    this.screenCenter = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+    this.mouse = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+    this.isHovering = false;
 
-    this.listeners()
-    this.animateCursor()
+    this.listeners();
+    this.animateCursor();
   }
   animateIn() {
-    return new Promise(resolve => {
-      const delay = .6
+    return new Promise((resolve) => {
+      const delay = 0.6;
       const timeline = gsap.timeline({
         defaults: {
-          duration: .8
+          duration: 0.8,
         },
         onComplete() {
-          resolve()
-        }
-      })
+          resolve();
+        },
+      });
       timeline
         .set(this.intro, { alpha: 0 })
-        .set(this.links, { y: -220, alpha: 0, width: 80, pointerEvents: 'none' })
-        .set(this.revealWrapper, { clipPath: 'polygon(0 0, 100% 0, 100% 0%, 0% 100%)' })
-        .set(this.revealText, { y: '100%' })
-        .set(this.links, {pointerEvents: 'all'}, delay + 1.4)
+        .set(this.links, {
+          y: -220,
+          alpha: 0,
+          width: 80,
+          pointerEvents: "none",
+        })
+        .set(this.revealWrapper, {
+          clipPath: "polygon(0 0, 100% 0, 100% 0%, 0% 100%)",
+        })
+        .set(this.revealText, { y: "100%" })
+        .set(this.links, { pointerEvents: "all" }, delay + 1.4)
         .to(this.intro, { alpha: 1 }, delay)
-        .to(this.revealWrapper, { clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)', ease: Power2.easeOut }, delay + .3)
-        .to(this.revealText, { y: 0, ease: Power2.easeOut }, delay + .2)
-        .to(this.links, { y: 0, alpha: 1, width: 160, ease: Power2.easeOut, stagger: .3,  }, delay + .8)
-    })
+        .to(
+          this.revealWrapper,
+          {
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+            ease: Power2.easeOut,
+          },
+          delay + 0.3
+        )
+        .to(this.revealText, { y: 0, ease: Power2.easeOut }, delay + 0.2)
+        .to(
+          this.links,
+          { y: 0, alpha: 1, width: 160, ease: Power2.easeOut, stagger: 0.3 },
+          delay + 0.8
+        );
+    });
   }
   listeners() {
     this.links.forEach((link, index) => {
-      link.addEventListener('mouseenter', this.enterLink.bind(this, index, link))
-      link.addEventListener('mouseleave', this.leaveLink.bind(this))
-    })
-    document.addEventListener('mousemove', e => {
-      this.mouse = { x: e.clientX, y: e.clientY }
-    })
-    window.addEventListener('resize', () => {
-      this.screenCenter = { x: window.innerWidth / 2, y: window.innerHeight / 2 }
-    })
+      link.addEventListener(
+        "mouseenter",
+        this.enterLink.bind(this, index, link)
+      );
+      link.addEventListener("mouseleave", this.leaveLink.bind(this));
+    });
+    document.addEventListener("mousemove", (e) => {
+      this.mouse = { x: e.clientX, y: e.clientY };
+    });
+    window.addEventListener("resize", () => {
+      this.screenCenter = {
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 2,
+      };
+    });
   }
   enterLink(i, item) {
-    this.isHovering = true
-    const title = item.getAttribute('data-link')
-    this.pagesCursor.querySelector('h3').innerHTML = title
-    this.pagesCursor.classList.add('active')
-    
+    this.isHovering = true;
+    const title = item.getAttribute("data-link");
+    this.pagesCursor.querySelector("h3").innerHTML = title;
+    this.pagesCursor.classList.add("active");
+
     this.links.forEach((link, index) => {
       if (i === 0 && index !== 0) {
-        gsap.to(link, .4, { alpha: 1 - index / 10 })
+        gsap.to(link, 0.4, { alpha: 1 - index / 10 });
       } else if (i === 1 && index !== 1) {
-        gsap.to(link, .4, { alpha: index < 3 ? .9 : .8 })
+        gsap.to(link, 0.4, { alpha: index < 3 ? 0.9 : 0.8 });
       } else if (i === 2 && index !== 2) {
-        gsap.to(link, .4, { alpha: index > 0 ? .9 : .8 })
+        gsap.to(link, 0.4, { alpha: index > 0 ? 0.9 : 0.8 });
       } else if (i === 3 && index !== 3) {
-        gsap.to(link, .4, { alpha: .7 + index / 10 })
+        gsap.to(link, 0.4, { alpha: 0.7 + index / 10 });
       }
-    })
+    });
   }
   leaveLink() {
-    this.isHovering = false
-    this.pagesCursor.classList.remove('active')
+    this.isHovering = false;
+    this.pagesCursor.classList.remove("active");
 
-    this.links.forEach(singleLink => {
-      gsap.to(singleLink, .4, { alpha: 1 })
-    })
+    this.links.forEach((singleLink) => {
+      gsap.to(singleLink, 0.4, { alpha: 1 });
+    });
   }
   animateCursor() {
     if (this.isHovering) {
-      const cursorCenterY = this.pagesCursor.offsetHeight / 2
-      const cursorCenterX = this.pagesCursor.offsetWidth / 2
-      gsap.to(this.pagesCursor, .2, {
+      const cursorCenterY = this.pagesCursor.offsetHeight / 2;
+      const cursorCenterX = this.pagesCursor.offsetWidth / 2;
+      gsap.to(this.pagesCursor, 0.2, {
         css: {
           left: this.mouse.x - cursorCenterX,
           top: this.mouse.y - cursorCenterY,
         },
-        ease: Power2.easeOut
-      })
+        ease: Power2.easeOut,
+      });
     }
 
-    const dx = this.mouse.x - this.screenCenter.x
-    const dy = this.mouse.y - this.screenCenter.y
+    const dx = this.mouse.x - this.screenCenter.x;
+    const dy = this.mouse.y - this.screenCenter.y;
 
-    const degree = (dx / this.screenCenter.x)
-    
+    const degree = dx / this.screenCenter.x;
+
     gsap.to(this.introTitle, 1, {
       rotateY: degree,
-      ease: Power2.easeOut
-    })
+      ease: Power2.easeOut,
+    });
 
-    gsap.to([this.pages, this.intro], 2, {
-      x: dx / this.screenCenter.x * -24,
-      y: dy / this.screenCenter.y * -16,
-      ease: Power2.easeOut
-    })
+    gsap.to(this.pages, 2, {
+      x: (dx / this.screenCenter.x) * -24,
+      y: (dy / this.screenCenter.y) * -16,
+      ease: Power2.easeOut,
+    });
 
-    requestAnimationFrame(this.animateCursor.bind(this))
+    requestAnimationFrame(this.animateCursor.bind(this));
   }
 }
